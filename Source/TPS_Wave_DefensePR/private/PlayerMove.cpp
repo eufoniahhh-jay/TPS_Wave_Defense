e1f0eb10a1d051c8c7bf52df3969736cbe049631
eufoniahhh-jay <eufoniahhh@gmail.com>
2025-12-08 17:48:51 +0900

    Day6: Refactored TPSPlayer into component-based structure (PlayerBaseComponent, PlayerMove, PlayerFire)

---
+++ Source/TPS_Wave_DefensePR/private/PlayerMove.cpp
@@ -0,0 +1,93 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerMove.h"
+#include "EnhancedInputComponent.h"
+#include "InputActionValue.h"
+
+UPlayerMove::UPlayerMove()
+{
+	// Tick 함수 호출되도록 처리
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UPlayerMove::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 초기 속도를 걷기 속도로 설정
+	moveComp->MaxWalkSpeed = walkSpeed;
+}
+
+void UPlayerMove::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	PlayerMove();
+}
+
+void UPlayerMove::SetupInputBinding(UEnhancedInputComponent* PlayerInput)
+{
+	PlayerInput->BindAction(ia_Turn, ETriggerEvent::Triggered, this, &UPlayerMove::Turn);
+	PlayerInput->BindAction(ia_LookUp, ETriggerEvent::Triggered, this, &UPlayerMove::LookUp);
+	PlayerInput->BindAction(ia_Move, ETriggerEvent::Triggered, this, &UPlayerMove::Move);
+	// 달리기 입력 이벤트 처리 함수 바인딩
+	PlayerInput->BindAction(ia_Run, ETriggerEvent::Started, this, &UPlayerMove::InputRun);
+	PlayerInput->BindAction(ia_Run, ETriggerEvent::Completed, this, &UPlayerMove::InputRun);
+	// 점프 입력 이벤트 처리 함수 바인딩
+	PlayerInput->BindAction(ia_Jump, ETriggerEvent::Started, this, &UPlayerMove::InputJump);
+}
+
+void UPlayerMove::Turn(const FInputActionValue& inputValue)
+{
+	float value = inputValue.Get<float>();
+	me->AddControllerYawInput(value);
+}
+
+void UPlayerMove::LookUp(const FInputActionValue& inputValue)
+{
+	float value = inputValue.Get<float>();
+	me->AddControllerPitchInput(value);
+}
+
+void UPlayerMove::Move(const struct FInputActionValue& inputValue)
+{
+	FVector2D value = inputValue.Get<FVector2D>();
+	// 상하 입력 이벤트 처리
+	direction.X = value.X;
+	// 좌우 입력 이벤트 처리
+	direction.Y = value.Y;
+}
+
+void UPlayerMove::PlayerMove()
+{
+	//	// 플레이어 이동 처리
+	// 등속 운동
+	// P(결과위치) = P0(현재위치) + v(속도) X t(시간)
+	direction = FTransform(me->GetControlRotation()).TransformVector(direction);
+	/*FVector P0 = GetActorLocation();
+	FVector vt = direction * walkSpeed * DeltaTime;
+	FVector P = P0 + vt;
+	SetActorLocation(P);*/
+	me->AddMovementInput(direction);
+	direction = FVector::ZeroVector;
+}
+
+void UPlayerMove::InputRun()
+{
+	auto movement = me->GetCharacterMovement();
+
+	// 현재 달리기 모드라면
+	if (movement->MaxWalkSpeed > walkSpeed) {
+		// 걷기 속도로 전환
+		movement->MaxWalkSpeed = walkSpeed;
+	}
+	else {
+		movement->MaxWalkSpeed = runSpeed;
+	}
+}
+
+void UPlayerMove::InputJump(const FInputActionValue& inputValue)
+{
+	me->Jump();
+}
--- Source/TPS_Wave_DefensePR/public/TPSPlayer.h
+++ Source/TPS_Wave_DefensePR/public/TPSPlayer.h
@@ -35,100 +35,18 @@ public:
 public:
 	UPROPERTY(EditDefaultsOnly, Category = "Input")
 	class UInputMappingContext* imc_TPS;
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	class UInputAction* ia_LookUp;
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	class UInputAction* ia_Turn;
-
-	// 좌우 회전 입력 처리
-	void Turn(const struct FInputActionValue& inputValue);
-	// 상하 회전 입력 처리
-	void LookUp(const struct FInputActionValue& inputValue);
-
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	class UInputAction* ia_Move;
-	// 걷기 속도
-	UPROPERTY(EditAnywhere, Category=PlayerSetting)
-	float walkSpeed = 200;
-	// 달리기 속도
-	UPROPERTY(EditAnywhere, Category = PlayerSetting)
-	float runSpeed = 600;
-	// 이동 방향
-	FVector direction;
-
-	void Move(const struct FInputActionValue& inputValue);
-
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	class UInputAction* ia_Jump;
-	// 점프 입력 이벤트 처리 함수
-	void InputJump(const struct FInputActionValue& inputValue);
-
-	// 플레이어 이동 처리
-	void PlayerMove();
-
+	
 	// 총 스켈레탈 메시
 	UPROPERTY(VisibleAnywhere, Category = GunMesh)
 	class USkeletalMeshComponent* gunMeshComp;
-
-	// 총알 공장
-	UPROPERTY(EditDefaultsOnly, Category=BulletFactory)
-	TSubclassOf<class ABullet> bulletFactory;
-
-	UPROPERTY(EditDefaultsOnly, Category="Input")
-	class UInputAction* ia_Fire;
-	// 총알 발사 처리함수
-	void InputFire(const struct FInputActionValue& inputValue);
 
 	// 스나이퍼건 스태틱매시 추가
 	UPROPERTY(EditAnywhere, Category = GunMesh)
 	class UStaticMeshComponent* sniperGunComp;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	class UInputAction* ia_GrenadeGun;
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	class UInputAction* ia_SniperGun;
-	// 유탄총 사용중인지 여부
-	bool bUsingGrenadeGun = true;
-	// 유탄총으로 변경
-	void ChangeToGrenadeGun(const struct FInputActionValue& inputValue);
-	// 스나이퍼건으로 변경
-	void ChangeToSniperGun(const struct FInputActionValue& inputValue);
-
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	class UInputAction* ia_Sniper;
-	// 스나이퍼 조준 처리 함수
-	void SniperAim(const struct FInputActionValue& InputValue);
-	// 스나이퍼 조준 중인지 여부
-	bool bSniperAim = false;
-	// 스나이퍼 UI 위젯 공장
-	UPROPERTY(EditDefaultsOnly, Category=SniperUI)
-	TSubclassOf<class UUserWidget> sniperUIFactory;
-	// 스나이퍼 UI 위젯 인스턴스
-	UPROPERTY()
-	class UUserWidget* _sniperUI;
-
-	// 총알 파편 효과 공장
-	UPROPERTY(EditAnywhere, Category=BulletEffect)
-	class UParticleSystem* bulletEffectFactory;
-
-	// 일반 조준 크로스헤어UI 위젯
-	UPROPERTY(EditDefaultsOnly, Category = SniperUI)
-	TSubclassOf<class UUserWidget> crosshairUIFactory;
-	// 크로스헤어 인스턴스
-	UPROPERTY()
-	class UUserWidget* _crosshairUI;
-
-	// 달리기 입력
-	UPROPERTY(EditDefaultsOnly, Category="Input")
-	class UInputAction* ia_Run;
-	// 달리기 이벤트 처리 함수
-	void InputRun();
-
-	// 카메라 셰이크 블루프린트를 저장할 변수
-	UPROPERTY(EditDefaultsOnly, Category=CameraMotion)
-	TSubclassOf<class UCameraShakeBase> cameraShake;
-
-	// 총알 발사 사운드
-	UPROPERTY(EditDefaultsOnly, Category=Sound)
-	class USoundBase* bulletSound;
+public:
+	UPROPERTY(VisibleAnywhere, Category = Component)
+	class UPlayerBaseComponent* playerMove;
+	UPROPERTY(VisibleAnywhere, Category = Component)
+	class UPlayerBaseComponent* playerFire;
 };
